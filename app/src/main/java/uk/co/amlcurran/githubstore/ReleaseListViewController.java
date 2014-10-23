de8f75ccd0af7c65062dc14ce1622e09e623720a
Alex Curran <aml.curran@gmail.com>
2014-10-23 21:39:20 +0100

    Move to list to show releases. Show body of releases

--- app/src/main/java/uk/co/amlcurran/githubstore/ReleaseListViewController.java
+++ app/src/main/java/uk/co/amlcurran/githubstore/ReleaseListViewController.java
@@ -1,26 +1,35 @@
 package uk.co.amlcurran.githubstore;
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import uk.co.amlcurran.viewcontroller.ViewController;
 
 public class ReleaseListViewController implements ViewController {
+
     private final GithubApi api;
-    private TextView releasesText;
+    private final RecyclerView.Adapter<ReleaseViewHolder> releasesAdapter;
+    private final List<Release> releaseList = new ArrayList<Release>();
 
     public ReleaseListViewController(GithubApi api) {
         this.api = api;
+        this.releasesAdapter = new ReleaseAdapter();
     }
 
     @Override
     public View inflateView(LayoutInflater layoutInflater, ViewGroup viewGroup) {
         View view = layoutInflater.inflate(R.layout.view_controller_release_list, viewGroup, false);
-        releasesText = ((TextView) view.findViewById(R.id.textView));
+        RecyclerView releasesListView = ((RecyclerView) view.findViewById(R.id.releases_list));
+        releasesListView.setLayoutManager(new LinearLayoutManager(viewGroup.getContext()));
+        releasesListView.setAdapter(releasesAdapter);
         return view;
     }
 
@@ -29,12 +38,8 @@ public class ReleaseListViewController implements ViewController {
         api.getReleases(new GithubApi.ResultListener<List<Release>>() {
             @Override
             public void received(List<Release> result) {
-                StringBuilder builder = new StringBuilder();
-                for (Release release : result) {
-                    builder.append(release.getTagName())
-                            .append("\n");
-                }
-                releasesText.setText(builder);
+                releaseList.addAll(result);
+                releasesAdapter.notifyItemRangeInserted(0, result.size());
             }
         });
     }
@@ -42,5 +47,38 @@ public class ReleaseListViewController implements ViewController {
     @Override
     public void stop() {
 
+    }
+
+    private class ReleaseViewHolder extends RecyclerView.ViewHolder {
+        private final TextView tagText;
+        private final TextView bodyText;
+
+        public ReleaseViewHolder(View itemView) {
+            super(itemView);
+            tagText = (TextView) itemView.findViewById(android.R.id.text1);
+            bodyText = ((TextView) itemView.findViewById(android.R.id.text2));
+            bodyText.setMaxLines(3);
+            bodyText.setEllipsize(TextUtils.TruncateAt.END);
+        }
+    }
+
+    private class ReleaseAdapter extends RecyclerView.Adapter<ReleaseViewHolder> {
+        @Override
+        public ReleaseViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+            View inflate = LayoutInflater.from(viewGroup.getContext()).inflate(android.R.layout.simple_list_item_2, viewGroup, false);
+            return new ReleaseViewHolder(inflate);
+        }
+
+        @Override
+        public void onBindViewHolder(ReleaseViewHolder releaseViewHolder, int i) {
+            Release release = releaseList.get(i);
+            releaseViewHolder.tagText.setText(release.getTagName());
+            releaseViewHolder.bodyText.setText(release.getBody());
+        }
+
+        @Override
+        public int getItemCount() {
+            return releaseList.size();
+        }
     }
 }
--- core/src/main/java/uk/co/amlcurran/githubstore/Release.java
+++ core/src/main/java/uk/co/amlcurran/githubstore/Release.java
@@ -2,8 +2,13 @@ package uk.co.amlcurran.githubstore;
 
 public class Release {
     private String tag_name;
+    private String body;
 
     public String getTagName() {
         return tag_name;
+    }
+
+    public String getBody() {
+        return body;
     }
 }
