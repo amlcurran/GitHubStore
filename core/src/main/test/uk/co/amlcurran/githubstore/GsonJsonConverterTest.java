32f2c16044e4f2f99024dc2dd0947e582115bebc
Alex Curran <aml.curran@gmail.com>
2014-10-24 18:35:28 +0100

    Add test around Json parsing to prevent a regression during this refactor

---
+++ core/src/main/test/uk/co/amlcurran/githubstore/GsonJsonConverterTest.java
@@ -0,0 +1,24 @@
+package uk.co.amlcurran.githubstore;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class GsonJsonConverterTest {
+
+    public static final String TAG_1 = "tag1";
+    public static final String BODY_2 = "body2";
+
+    @Test
+    public void testJsonParsedCorrectly() {
+        GsonJsonConverter converter = new GsonJsonConverter();
+        List<Release> releases = converter.convertReleases("[ { 'tag_name' : '" + TAG_1 + "', 'body' : 'body1' }, { 'tag_name' : 'tag2', 'body' : '" + BODY_2 + "' } ]");
+        assertThat(releases.get(0).getTagName(), is(TAG_1));
+        assertThat(releases.get(1).getBody(), is(BODY_2));
+    }
+
+
+}
